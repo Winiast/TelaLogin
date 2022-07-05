05e69bf4df0ece05456d0c240d891c14da228a76
Gustavo Ferreira <82901156+Winiast@users.noreply.github.com>
2022-07-05 00:05:26 -0300

    Erro porém com atualizações

---
+++ src/LoginController.java
@@ -0,0 +1,30 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Button buttonEnter;
+
+    @FXML
+    private Button buttonRegister;
+
+    @FXML
+    private TextField campUserPassoword;
+
+    @FXML
+    private TextField campoUserName;
+
+    @FXML
+    void fazerCadastro(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fazerLogin(ActionEvent event) {
+
+    }
+
+}
